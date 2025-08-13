5b356f39709dacaec8d654d55b9be68465d7db02
yonatmiller <yonatmiller@gmail.com>
2025-08-13 06:37:30 +0300

    Update photos

--- js/memeController.js
+++ js/memeController.js
@@ -1,13 +1,15 @@
-var gImgs = [{id: 1, url: '/img/putin.jpg', keywords: ['funny', 'cat']},
-            {id: 2, url: '/img/patrick.jpg', keywords: ['funny', 'cat']},
-            {id: 3, url: '/img/girl.jpg', keywords: ['funny', 'cat']},
-            {id: 4, url: '/img/baby.jpg', keywords: ['funny', 'cat']},
-            {id: 5, url: '/img/X-Everywhere.jpg', keywords: ['funny', 'cat']}]
+var gImgs = [{id: 1, url: 'img/putin.jpg', keywords: ['funny', 'cat']},
+            {id: 2, url: 'img/patrick.jpg', keywords: ['funny', 'cat']},
+            {id: 3, url: 'img/girl.jpg', keywords: ['funny', 'cat']},
+            {id: 4, url: 'img/baby.jpg', keywords: ['funny', 'cat']},
+            {id: 5, url: 'img/X-Everywhere.jpg', keywords: ['funny', 'cat']}]
+
 
 var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
 
 function renderMeme(){
     getMeme()
+    renderLine()
 }
 
 function onDownloadCanvas(elLink){
@@ -21,4 +23,51 @@ function onSEetFont(dif){
     renderMeme()
 }
 
+function onShowLines(ev) {
+    const { offsetX, offsetY, clientX, clientY } = ev
+
+    const clickedLine = gMeme.lines.find(line => {
+        const xLine = line.pos.x
+        const yLine = line.pos.y
+
+        return (
+            offsetX >= xLine &&
+            offsetX <= xLine + textWidth(line) &&
+            offsetY >= yLine &&
+            offsetY <= yLine + line.size)
+    })
+
+    if (clickedLine) {
+        // Todo: Fix modal location relative to screen
+        const idx = gMeme.lines.findIndex(line => line.txt === clickedLine.txt);
+        gMeme.selectedLineIdx = idx
+        const elTextInput = document.querySelector('.canvas-text')
+        elTextInput.value = gMeme.lines[gMeme.selectedLineIdx].txt
+
+        moveLine()
+    }
+}
+
+function renderLine(){
+    gCtx.strokeStyle = 'black';
+    gCtx.lineWidth = 2
+    gCtx.strokeRect(gLine.pos.x,gLine.pos.y,gLine.width, gLine.height )
+}
+
+function onAddLine(){
+    console.log('hi');
+    
+    var newLine = {
+        pos:{
+            x: 0,
+            y:40,
+        }, 
+        txt: 'new line',
+        size: 20,
+        color: 'black'
+        }
+    gMeme.lines.push(newLine)
+
+    renderMeme()
+}
 
--- js/memeService.js
+++ js/memeService.js
@@ -2,29 +2,56 @@ var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [{
+        pos:{
+            x: 0,
+            y:0,
+        },
         txt: 'I sometimes eat Falafel',
+        size: 20,
+        color: 'red'
+        },
+        {  
+        pos:{
+            x: 0,
+            y:20,
+        }, 
+        txt: 'I love you',
         size: 20,
         color: 'red'
         }]
 }
 
+var gLine = {
+    pos:{
+        x:0,
+        y:0,
+    },
+    height: 20,
+    width: 20,
+    color: 'black',
+}
+
 function getMeme(){
     const elImg = new Image()
     var img = gImgs.find((img) => img.id === gMeme.selectedImgId)
-    
     elImg.src = img.url
+    
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
-        gCtx.font = gMeme.lines[gMeme.selectedLineIdx].size + 'px Arial'
-        gCtx.fillStyle = gMeme.lines[gMeme.selectedLineIdx].color
+
+        gMeme.lines.forEach(line => {
+        gCtx.font = `${line.size}px Arial`
+        gCtx.fillStyle = line.color
         gCtx.textBaseline = 'top'
         gCtx.textAlign = 'left'
-        gCtx.fillText(`${gMeme.lines[0].txt}`, 0, 0)
+        gCtx.fillText(line.txt, line.pos.x, line.pos.y)})
     }
 }
 
 function setLineTxt(){
     const elTextInput = document.querySelector('.canvas-text')
+    console.log(gMeme.lines[gMeme.selectedLineIdx].txt);
+    
     gMeme.lines[gMeme.selectedLineIdx].txt = elTextInput.value
     
     renderMeme()
@@ -35,9 +62,32 @@ function setImg(imgId){
     renderMeme()
 }
 
-function updateAll(event) {
-  document.querySelectorAll("p").forEach((p) => {
-    p.style.color = event.target.value;
-  });
+const elColorInput = document.querySelector('.brushColor')
+
+elColorInput.addEventListener('input', (ev) => {
+  const color = ev.target.value;
+  gMeme.lines[gMeme.selectedLineIdx].color = color
+
+  renderMeme()
+})
+
+
+function textWidth(){
+    
+    
+    const text = gMeme.lines[gMeme.selectedLineIdx].txt
+    gCtx.font = `${gMeme.lines[gMeme.selectedLineIdx].size}px Arial`
+    const textMetrics = gCtx.measureText(text)
+    const textWidth = textMetrics.width
+
+    return textWidth
 }
 
+function moveLine(){
+    gLine.pos.x = gMeme.lines[gMeme.selectedLineIdx].pos.x
+    gLine.pos.y = gMeme.lines[gMeme.selectedLineIdx].pos.y
+    gLine.width = textWidth()
+    gLine.height = gMeme.lines[gMeme.selectedLineIdx].size
+
+    renderLine()
+}
